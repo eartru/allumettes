8b54d170335fbc0a33f8026cfe979e1e9c929308
basile <basilelegal@gmail.com>
2018-12-12 15:51:11 -0500

    Adds comment for IA and README

--- README.md
+++ README.md
@@ -1,1 +1,75 @@
 # allumettes
+
+
+## Setup
+
+__required__
+
+  * Python 3.5
+  * Virtualenv
+  * pip
+
+*For Linux users:*
+
+First check your python3 version
+
+```
+python3 -V
+```
+
+if it's lower than 3.5 install it.
+If you don't have `pip` -> `sudo apt-get install python3-pip`
+
+Then you need virtualenv
+
+```
+pip3 install virtualenv
+```
+
+*For mac users:*
+
+First you will need `brew` https://brew.sh/2018/01/19/homebrew-1.5.0/ , then:
+
+```
+brew install pip3
+```
+Then you need virtualenv
+
+```
+pip3 install virtualenv
+```
+
+__virtualenv setup__
+
+In the allumettes directory
+
+```
+virtualenv env
+source env/bin/activate
+pip3 install pygame
+pip3 install colorama
+```
+
+## Play the game
+
+`python3 main.py`
+
+Inside the game to pick a number of matches:
+   with a azerty keyboard:
+     - "&" = 1
+     - "é" = 2
+     - " (just the key " ) = 3
+    with any other kind of keyboard
+     - "Q" = 1
+     - "W" = 2
+     - "E" = 3
+
+     To leave the game press "A"
+
+## Benchmark and compare IA
+
+To play with the different kind of IA
+
+`python3 ia_arena.py`
+
+*WARNING: Be careful these algorythm are not optimise and have a heavy cost avoid going higher than 25 matches*
--- allumettes/game/IA.py
+++ allumettes/game/IA.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 from random import randint
 from game.utils import timing
 from colorama import Fore
@@ -5,6 +7,12 @@ from colorama import Style
 
 
 class RandomIA(object):
+
+    '''
+        Simple IA  which returns only random number between 1 and 3
+        Cannot return a number higher than the number of matches left
+        left on the board.
+    '''
 
     @timing
     def take_decision(self, nb_match):
@@ -15,7 +23,34 @@ class RandomIA(object):
         return 'Random IA'
 
 
+class MathIA(object):
+    '''
+        Simple IA which returns the perfect number according the number
+        of matches left on the board. The Solution to win this game is to left
+        a total of matches equals to a multiple of 4 + 1 on the board so your
+        opponent can never win. The flaw is when your opponent knows this trick
+        and is the first to play you can't do anything so this IA returns 1 in
+        this condition.
+    '''
+
+    @timing
+    def take_decision(self, nb_match):
+        for i in range(1, 4):
+            nb_match_left = nb_match - i
+            if nb_match_left % 4 == 1:
+                return i
+        return 1
+
+    def __str__(self):
+        return 'Math IA'
+
+
 class MinMaxIA(object):
+
+    '''
+        This IA will retun the value of the node which has this higher weight.
+        The weight of the node the probability to win.
+    '''
 
     @timing
     def take_decision(self, nb_match):
@@ -32,6 +67,11 @@ class MinMaxIA(object):
 
 class GraphMinMaxIA(object):
 
+    '''
+        This IA will retun the value of the referenced node which has this
+        higher weight. The weight of the node the probability to win.
+    '''
+
     @timing
     def take_decision(self, nb_match):
         root = ReferencedNode(nb_match)
@@ -39,24 +79,23 @@ class GraphMinMaxIA(object):
         return nb_match - root.get_max_node().match_left
 
 
-class MathIA(object):
-
-    @timing
-    def take_decision(self, nb_match):
-        for i in range(1, 4):
-            nb_match_left = nb_match - i
-            if nb_match_left % 4 == 1:
-                return i
-        return 1
-
-    def __str__(self):
-        return 'Math IA'
-
-
 class Node(object):
 
+    '''
+        This object is the representation of a Min Max tree, for a given
+        number of matches.
+        params:
+            - visited_node: number of node created/visited to find a solution
+            - children: list of direct descendant of the current node
+            - weight: probability to win at the current node, aggegation of the
+                      weight of all the node's children
+            - depth: generation of node
+                     also number of parent of the current node
+            - match_left: represention of number of matches left on
+                          the board at the current node
+    '''
+
     visited_node_count = 1
-    children = None
     weight = 0
 
     def __init__(self, match_left, depth=0, value=0):
@@ -68,6 +107,11 @@ class Node(object):
             self.weight = 1 if self.depth % 2 == 0 else -1
 
     def make_children(self):
+
+        '''
+            Recursive function that creates all the nodes, links them to
+            their parents and aggregate their weight
+        '''
         i = 1
         while i < 4 and (self.match_left - i >= 0):
             node = Node(self.match_left - i, self.depth + 1, i)
@@ -93,7 +137,16 @@ references = dict()
 
 class ReferencedNode(object):
 
-    get_weight = None
+    """
+        Graph representation of the Min Max Tree, aggregate all path's length to
+        go from x to y and give them a weight of 1 if they're odd or -1 if not.
+
+        params
+            - children: list of direct descendant of the current node
+            - match_left: represention of number of matches left on
+                          the board at the current node
+            - routes: list of path's length to go from match_left to 0
+    """
 
     def __init__(self, match_left):
         self.children = []
